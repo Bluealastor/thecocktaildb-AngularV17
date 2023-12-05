680b4d598d9b1812335e6058317805550e5aa5cd
Christian <chrise79456@gmail.com>
2023-12-05 18:24:22 +0100

    cards Restylings component

--- src/app/components/cocktail-card-component/cocktail-card-component.component.ts
+++ src/app/components/cocktail-card-component/cocktail-card-component.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
 
 @Component({
   selector: 'app-cocktail-card-component',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatIconModule],
+  imports: [CommonModule, RouterModule,MatCardModule, MatButtonModule],
   templateUrl: './cocktail-card-component.component.html',
   styleUrl: './cocktail-card-component.component.css'
 })
@@ -15,6 +17,8 @@ export class CocktailCardComponentComponent implements OnInit{
   isCardFound: boolean = true; 
   idVariabile: string = 'idDinamico';
   isFavorite: boolean = false;
+  textResponsive: string = "";
+  lenghtText: number= 100;
 
   // Controlla se l'ID corrente è tra quelli nel localStorage
   buttonImage: string = '../../../assets/heart.png'; // Immagine predefinita per il pulsante
@@ -37,17 +41,13 @@ export class CocktailCardComponentComponent implements OnInit{
   gestisciPreferito(): void {
     const idsString = localStorage.getItem('storedIDs');
     let arrayDiIDs: string[] = idsString ? JSON.parse(idsString) : [];
-
     if (this.isFavorite) {
       arrayDiIDs = arrayDiIDs.filter(id => id !== this.cocktail.idDrink);
     } else {
       arrayDiIDs.push(this.cocktail.idDrink);
     }
-
     localStorage.setItem('storedIDs', JSON.stringify(arrayDiIDs));
-
     this.isFavorite = !this.isFavorite;
-
     // Cambia l'immagine del pulsante dopo aver aggiornato 'isFavorite'
     if (this.isFavorite) {
       this.buttonImage = '../../../assets/heartred.png';
@@ -55,6 +55,17 @@ export class CocktailCardComponentComponent implements OnInit{
       this.buttonImage = '../../../assets/heart.png';
     }
   }
+
+  responsiveText(text: string): string{
+    if (text.length > this.lenghtText){
+      return text.substring(0, this.lenghtText) + "..."
+    }
+    return text
+  }
+
+
+
+
   ngOnInit(): void {
   // console.log(this.cocktail)
   // console.log(localStorage.getItem('storedIDs'))
